caaa9a5e2860160b403b8bcdb459555f63bdbfaa
Matzaa Wahorn <matzaa@matzaa.com>
2020-06-08 13:56:28 +0200

    trying to deploy

--- src/contact.js
+++ src/contact.js
@@ -4,19 +4,22 @@ export default function Contact() {
     return (
         <div id="contact">
             <div id="contact-container">
-                <h3>Contact me</h3>
-
-                <i className="far fa-paper-plane"></i>
-                <a
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    href="https://github.com/Matzaa"
-                >
-                    <i className="fab fa-github"></i>
-                </a>
-                <a>
-                    <i className="fab fa-linkedin"></i>
-                </a>
+                <h3 className="project-title">Contact me</h3>
+                <div id="links">
+                    <a href="mailto:matzaa@matzaawahorn.com">
+                        <i className="far fa-paper-plane"></i>
+                    </a>
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://github.com/Matzaa"
+                    >
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a>
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                </div>
             </div>
         </div>
     );
--- src/portfolio.js
+++ src/portfolio.js
@@ -24,7 +24,7 @@ export default class Portfolio extends React.Component {
         this.setState({ modal: !this.state.modal });
     }
     setIB() {
-        this.setState({ url: "./ib-preview" });
+        this.setState({ url: "./ib-preview.mov" });
         this.setState({ modal: !this.state.modal });
     }
 
@@ -48,21 +48,50 @@ export default class Portfolio extends React.Component {
 
                 <div className="project-container">
                     <div className="text">
-                        <h3>Social Network</h3>
+                        <h3 className="project-title">Social Network</h3>
 
                         <p>
                             FilmBook is a single page application that allows
                             users to join a social network focusing on a love of
-                            cinema. Users can register, add and update their
-                            profile picture and biography, send and receive
-                            friend requests, add and remove their favorite
-                            movies with the help of the Open Movie Database API,
-                            comment on movies, leave messages on eachother's
-                            wall, connect with others based on liked movies.
+                            cinema. Users can
+                            <ul>
+                                <li> register and login</li>
+                                <li>
+                                    get email with secret code to reset
+                                    forgotten password with AWS SES
+                                </li>
+                                <li>
+                                    add and/or update profile picture and
+                                    biography
+                                </li>
+                                <li>send and receive friend requests</li>
+                                <li>
+                                    add and remove favorite movies with the help
+                                    of the Open Movie Database API
+                                </li>
+                                <li>
+                                    comment and engage in conversation on each
+                                    movie's page
+                                </li>
+                                <li>leave messages on eachother's wall</li>
+                                <li>
+                                    connect with others based on favorite movies
+                                </li>
+                            </ul>
                         </p>
                         <p>
                             Technologies: React.js, Redux, Socket.io,
                             JavaScript, AWS S3, PostgreSQL, Node.js
+                        </p>
+                        <p className="code">
+                            <a
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                href="https://github.com/Matzaa/imdb-social-network"
+                            >
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <p> see code!</p>
                         </p>
                     </div>
                     <a onClick={() => this.setSN()}>
@@ -72,16 +101,34 @@ export default class Portfolio extends React.Component {
                 </div>
                 <div className="project-container">
                     <div className="text">
-                        <h3>Image Board</h3>
+                        <h3 className="project-title">Image Board</h3>
                         <p>
                             Pretty red things is a single page application
-                            inspired by instagram, where anyone can post a
-                            picture with a title and description, as well as
-                            comment on any of the pictures.
+                            inspired by instagram. Users can
+                            <ul>
+                                <li>
+                                    post pictures with a title and description
+                                </li>
+                                <li>
+                                    comment on any of the pictures and see other
+                                    comments
+                                </li>
+                                <li>infinite scroll</li>
+                            </ul>
                         </p>
                         <p>
-                            Technologies: Vue.js, JavaScript, Node.js,
+                            Technologies: Vue.js, JavaScript, Express, Node.js,
                             PostgreSQL, AWS S3{" "}
+                        </p>
+                        <p className="code">
+                            <a
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                href="https://github.com/Matzaa/imageboard"
+                            >
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <p> see code!</p>
                         </p>
                     </div>
                     <a onClick={() => this.setIB()}>
@@ -91,28 +138,42 @@ export default class Portfolio extends React.Component {
                 </div>
                 <div className="project-container">
                     <div className="text">
-                        <h3>Petition</h3>
+                        <h3 className="project-title">Petition</h3>
                         <p>
                             This project is an online petition where users can
-                            register to sign a petition. Some features include
-                            adding signature on canvas, updating profile
-                            information, login with password, list of other
-                            signers sorted by city.
+                            register to sign a petition. Features:
+                            <ul>
+                                <li>register and login with hashed password</li>
+                                <li>add personal signature on canvas</li>
+                                <li>update profile information</li>
+                                <li>
+                                    see list of other signers, also sorted by
+                                    city
+                                </li>
+                            </ul>
                         </p>
                         <p>
                             Technologies: Handlebars, JavaScript, Node.js,
                             Express, PostgreSQL, Cookie Session{" "}
                         </p>
+                        <p className="code">
+                            <a
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                href="https://github.com/Matzaa/petition"
+                            >
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <p> see code!</p>
+                        </p>
                         <a
+                            className="visit"
                             rel="noopener noreferrer"
                             target="_blank"
                             href="https://neighborspetition.herokuapp.com/"
                         >
                             visit site!
                         </a>
-                        <p>
-                            see code <i className="fab fa-github"></i>
-                        </p>
                     </div>
 
                     <a
@@ -126,12 +187,25 @@ export default class Portfolio extends React.Component {
                 </div>
                 <div className="project-container">
                     <div className="text">
-                        <h3>Spotify Search</h3>
+                        <h3 className="project-title">Spotify Search</h3>
                         <p>
                             A page where you can search the spotify database
-                            through its API, featuring infinite scroll.
+                            <ul>
+                                <li>connects to the Spotify API</li>
+                                <li>features infinite scroll</li>
+                            </ul>
                         </p>
                         <p>Technologies: JavaScript, JQuery, AJAX </p>
+                        <p className="code">
+                            <a
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                href="https://github.com/Matzaa/spotify-search"
+                            >
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <p> see code!</p>
+                        </p>
                     </div>
                     <a onClick={() => this.setSpoti()}>
                         <img src="./spotify.png" />
@@ -140,7 +214,7 @@ export default class Portfolio extends React.Component {
                 </div>
                 <div className="project-container">
                     <div className="text">
-                        <h3>Connect Four</h3>
+                        <h3 className="project-title">Connect Four</h3>
                         <p>
                             An online version of the all-time favorite connect
                             four game where two players take turns dropping
@@ -149,10 +223,21 @@ export default class Portfolio extends React.Component {
                             vertically or diagonnaly wins the game.
                         </p>
                         <p>Technologies: JavaScript, JQuery </p>
+                        <p className="code">
+                            <a
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                href="https://github.com/Matzaa/connect-four"
+                            >
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <p> see code!</p>
+                        </p>
                         <a
+                            className="visit"
                             rel="noopener noreferrer"
                             target="_blank"
-                            href="https://matzaa.github.io/connect-four/"
+                            href="https://matzaa.github.io/connect-four//"
                         >
                             visit site!
                         </a>
